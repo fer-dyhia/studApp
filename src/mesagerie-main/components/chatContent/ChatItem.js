d5a3e04098e88c5225dd7304bfa072c9912bf8a8
Mohandourad <45826498+Mohandourad@users.noreply.github.com>
2021-04-03 20:13:44 +0100

    moo

--- src/mesagerie-main/components/chatContent/ChatItem.js
+++ src/mesagerie-main/components/chatContent/ChatItem.js
@@ -11,7 +11,7 @@ const ChatItem = (props) => {
       {props.user == "me" ? (
         <div className="flex flex-row items-end">
           <div className="chat__item__content">
-            <div className="chat__msg ">{props.msg}</div>
+            <div className="chat__msg pb-2">{props.msg}</div>
 
             <ImgSent />
             <div className="chat__meta">
@@ -26,7 +26,7 @@ const ChatItem = (props) => {
           <Avatar isOnline="active" image={props.image} />
 
           <div className="chat__item__content">
-            <div className="chat__msg ">{props.msg}</div>
+            <div className="chat__msg pb-2">{props.msg}</div>
             <ImgSent />
             <div className="chat__meta">
               <span></span>
--- src/mesagerie-main/components/chatContent/ImgSent.js
+++ src/mesagerie-main/components/chatContent/ImgSent.js
@@ -2,7 +2,7 @@ import React from "react"
 import logo from "../../images/logo.png"
 const ImgSent =() => {
     return (
-        <div className="h-44 w-44">
+        <div className="h-44 w-48">
             <img src={logo} className="w-full h-full rounded-lg"/>
         </div>
     )
