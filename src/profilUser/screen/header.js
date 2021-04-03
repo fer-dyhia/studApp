afaaa462118cca3171b1d5d81ce639e38590ba49
Mohandourad <45826498+Mohandourad@users.noreply.github.com>
2021-04-03 21:55:42 +0100

    moo

--- src/profilUser/screen/header.js
+++ src/profilUser/screen/header.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Ident from '../header/ident'
-import Funct from '../header/funct'
+import Ident from "../header/ident";
+import Funct from "../header/funct";
+import { RiHomeSmileFill } from "react-icons/ri";
+import { useHistory } from "react-router";
 
-import profilepic from '../../img/me.jpg'
+import profilepic from "../../img/me.jpg";
 
 const Header = (props) => {
-    return (
-        <div class='relative flex flex-row shadow-lg h-48 w-full'>
-            <img className='absolute w-full h-full z-0' src={props.cover} />
+  const history = useHistory();
+  return (
+    <div class="relative flex flex-row shadow-lg h-48 w-full">
+      <button
+        className="z-50 absolute ml-72 left-96 top-6 bg-white bg-opacity-50 rounded-full w-16 h-16 px-4 focus:outline-none"
+        onClick={() => history.push("/filActualite")}
+      >
+        <RiHomeSmileFill className="text-blue-500 text-3xl" />
+      </button>
 
-            <Ident name={props.name} domain='Computer Science Student' img={props.img} />
+      <img className="absolute w-full h-full z-0" src={props.cover} />
 
-            <Funct username={props.name} />
-        </div>
-    )
-}
+      <Ident
+        name={props.name}
+        domain="Computer Science Student"
+        img={props.img}
+      />
 
-export default Header
+      <Funct username={props.name} />
+    </div>
+  );
+};
+
+export default Header;
