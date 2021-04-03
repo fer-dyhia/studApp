d3640c3d8b4adc901f08c08de15db797834b9764
Mohandourad <45826498+Mohandourad@users.noreply.github.com>
2021-04-03 19:37:49 +0100

    moo

--- src/mesagerie-main/components/chatContent/ImgComponent.js
+++ src/mesagerie-main/components/chatContent/ImgComponent.js
@@ -3,11 +3,9 @@ import ImageSending from "./ImageSending.js";
 
 const ImgComponent = () => {
   return (
-    <div className="flex flex-row absolute w-96 h-20 items-center justify-start bottom-10 left-16 bg-gray-50 bg-opacity-75 rounded">
+    <div className="flex flex-row absolute w-96 h-20 items-center justify-start bottom-10 left-16 rounded">
       <ImageSending />
-      <ImageSending />
-      <ImageSending />
-      <ImageSending />
+      
     </div>
   );
 };
